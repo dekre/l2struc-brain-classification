b2fe5e082565877bc9c0e0ce294c0f6b1e5c27d8
Denis Kreibel <deniskreibel@kpmg.es>
2021-02-21 13:03:30 +0100

    update preprocessing

---
+++ src/preprocessing/steps.py
@@ -0,0 +1,58 @@
+import numpy as np
+import tensorflow as tf
+import zipfile as zip_
+from pydicom import dcmread
+from sklearn.model_selection import GroupKFold
+import preprocessing.utils as utl
+
+
+def normalize(x: np):
+    return  x / x.max()
+
+
+
+def kfold_train_val_split(
+        src_zip_path: str, 
+        src_lbl_path: str, 
+        cross_valid_n_splits: int = 2
+    ):
+    src_zip = zip_.ZipFile(src_zip_path)
+    df_base = get_data(src_zip, src_lbl_path)
+
+    # Prepare cross validation sets    
+    group_kfold = GroupKFold(cross_valid_n_splits=2)
+    group_kfold.get_n_splits(df_base.file.values, df_base.y.values, df_base.subject.values)
+
+    # generate data
+    for train_index, test_index in group_kfold.split(df_base.file.values, df_base.y.values, df_base.subject.values):        
+        X_train, X_test = df_base.file.values[train_index], df_base.file.values[test_index]
+        y_train, y_test = df_base.y.values[train_index], df_base.y.values[test_index]
+        yield X_train, X_test, y_train, y_test
+    
+
+def brain_dataset(
+        X: np.ndarray,
+        y: np.ndarray,
+        src_zip_path: str,         
+        brain_slice_pos: tuple, 
+        brain_segments: tuple = (8,8),        
+    ):
+
+    src_zip = zip_.ZipFile(src_zip_path)
+    for file, label in zip(X, y):
+        source_pixel_array = utl.get_brain_image_pixel_array(src_zip, file)
+        target_pixel_array = get_brain_slice(source_pixel_array, brain_slice_pos, brain_segments)
+        yield label, target_pixel_array
+
+
+def brain_tf_dataset(data: brain_dataset, img_height: int = 128, img_width: int = 128):
+
+    dataset = (
+        tf.data.Dataset.from_generator(
+            data,        
+            output_signature=(            
+                tf.TensorSpec(shape=(), dtype=tf.int32),
+                tf.TensorSpec(shape=(img_height, img_width), dtype=tf.float64)
+            )
+        )
+    )
---
+++ src/preprocessing/utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import zipfile as zip
+import pandas as pd
+from pydicom import dcmread
+
+def get_brain_slice(pixel_array: np.ndarray, pos: tuple = (0,0), segments: tuple = (8,8)):    
+    total_pxiel = np.array(pixel_array.shape)
+    pos, segments = np.array(pos), np.array(segments)
+    rng_start = (total_pxiel / segments * pos).astype(int)
+    rng_end = (rng_start + total_pxiel / segments).astype(int)    
+    return pixel_array[rng_start[0]:rng_end[0],rng_start[1]:rng_end[1]]
+
+
+def get_brain_image_pixel_array(src_zip: zip.ZipFile, img_path: str):
+    with src_zip.open(img_path, "r") as f:
+        ds = dcmread(f)
+        f.close()
+    return ds.pixel_array
+
+
+def get_labels(lables_path: str) -> pd.DataFrame:
+    data = pd.read_csv(lables_path, sep = ";")
+    data["y"] = 0
+    data.loc[data.group == "L2", "y"] = 1
+    data.loc[data.group.isnull(), "y"] = np.NaN
+    return data
+
+def get_data(srczip: zip.ZipFile, lables_path: str) -> pd.DataFrame:    
+    data = get_labels(lables_path)
+    tmp = list()
+    for file in srczip.namelist():
+        if file.endswith(".dcm"):
+            subject = file.split("/")[1]
+            tmp.append((int(subject), file))
+    data = (
+        data
+        .merge(
+            pd.DataFrame(tmp, columns = ['subject', 'file']),
+            on = "subject"            
+        )
+    )
+    return data
+            
